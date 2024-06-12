01d14a18e3e712980d5a9de68eb58d2af7c9353f
abdulmoizsheraz <moiz77131@gmail.com>
2024-06-12 19:45:52 +0500

    feat:init

---
+++ my-app/app/(main)/(routes)/page.tsx
@@ -0,0 +1,16 @@
+
+import {
+  UserButton,
+} from "@clerk/nextjs";
+import { ModeToggle } from "@/components/ui/mode-toggle-ui";
+export default function Home() {
+  return (
+<div className="flex flex-col justify-center items-center">
+  <UserButton
+  afterSignOutUrl="/"
+  />
+  This is Protected Route accessible only to logged in users
+  <ModeToggle/>
+</div>
+  );
+}
