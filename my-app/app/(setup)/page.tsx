b958d1ea1e44906d1f07177a5b9ec7952d827c6a
abdulmoizsheraz <moiz77131@gmail.com>
2024-06-15 17:42:23 +0500

    feat:progrssing..

--- my-app/app/(setup)/page.tsx
+++ my-app/app/(setup)/page.tsx
@@ -1,8 +1,31 @@
+import { initialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import InitialModal from "@/components/modals/initial-modal"
 
 const SetupPage = async() => {
-  return (
-    <div>Create a Server</div>
-  )
-}
+  const profile = await initialProfile();
 
-export default SetupPage
+  // checking if the user is part of any server
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id, // Changed semicolon to comma
+        }
+      }
+    }
+  });
+
+
+  // if user is already part of server then redirect to that server
+  if (server) {
+    return redirect(`/server/${server.id}`); // Added parentheses to redirect
+  }
+
+  return (
+    <div><InitialModal/></div>
+  );
+};
+
+export default SetupPage;
